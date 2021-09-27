42b37bca1bf9a65522ff67f58da24ee09d30778a
tlaze <tomlazore@gmail.com>
2021-09-27 05:33:44 -0400

    New city inputs are stored in local storage as an object array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
-console.log("linked");
+function citySearch(){
+    
+    var userInput = $("#cityName").val();
+    console.log(userInput);
 
+    var cityObj = {
+        city: userInput,
+    }
 
-function clicked(){
-    console.log("clicked");
+    toLocalStorage(cityObj);
 }
+
+function toLocalStorage(newCity){
+
+    var cityArray = JSON.parse(localStorage.getItem('City'));
+
+    //save input to local storage
+    if(cityArray == null){
+        localStorage.setItem('City',JSON.stringify([]));
+        toLocalStorage(newCity);
+    }
+    else{
+        cityArray.push(newCity);
+        localStorage.setItem('City', JSON.stringify(cityArray));
+    }
+
+}
+
+//create and display list of stored inputs
+//get weather data when clicking previous inputs
+//make header with bootstrap
