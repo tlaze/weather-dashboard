3c831d2a4ec0e3d76ce1f6f469d4f72db953cd3c
tlaze <tomlazore@gmail.com>
2021-09-27 21:00:15 -0400

    set up API with user input and a key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,22 @@
+var openWeatherKey = "e3b4d1333323f6175037fc68166fb554";
 $("#searchButton").click(citySearch);
-var count = 0;
+
 function citySearch(){
 
-        
-        console.log("clickled");
-        
-        var userInput = $("#cityName").val();
-        
-        var cityObj = {
-            city: userInput,
-        }
-        
+    var userInput = $("#cityName").val();
+    
+    var cityObj = {
+        city: userInput,
+    }
+    
     toLocalStorage(cityObj);
     previousSearches();
+    retrieveData(cityObj);
 }
 
 function toLocalStorage(newCity){
 
     var cityArray = JSON.parse(localStorage.getItem('City'));
-    console.log(cityArray);
 
     //save input to local storage
     if(cityArray == null){
@@ -32,16 +30,24 @@ function toLocalStorage(newCity){
 }
 
 function previousSearches(){
-    console.log(count);
+;
     var displayedCities = JSON.parse(localStorage.getItem('City'));
-    var listItem = document.createElement("button");
-    var listText = document.createTextNode(displayedCities[count].city);
-    listItem.appendChild(listText);
-    document.getElementById("cityList").appendChild(listItem);
-    count++;
+
+    for(var i = 0; i < displayedCities.length; i++){
+        var listItem = document.createElement("button");
+        var listText = document.createTextNode(displayedCities[i].city);
+        listItem.appendChild(listText);
+        document.getElementById("pastSearchBox").appendChild(listItem).className = "newButtons btn btn-primary btn-sm col-12";
+    }
 }
 
+function retrieveData(currentCity){
+    var city = currentCity.city;
+    console.log(city);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + openWeatherKey;
 
+    fetch(queryURL);
+}
 
 //create and display list of stored inputs
 //get weather data when clicking previous inputs
